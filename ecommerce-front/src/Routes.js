9bca8158f4f65df45fad585ba0feb07bcbf5377a
Mai Quang Nhat <quangnhat224@gmail.com>
2020-05-06 15:09:53 +0300

    admin dashboard

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -5,6 +5,8 @@ import Signin from './user/Signin'
 import Home from './core/Home';
 import PrivateRoute from './auth/PrivateRoute';
 import Dashboard from './user/UserDashboard';
+import AdminRoute from './auth/AdminRoute';
+import AdminDashboard from './user/AdminDashboard';
 
 const Routes = () => {
     return (
@@ -13,7 +15,16 @@ const Routes = () => {
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <Route path="/" exact component={Home} />
-                <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+                <PrivateRoute
+                    path="/user/dashboard"
+                    exact
+                    component={Dashboard}
+                />
+                <AdminRoute
+                    path="/admin/dashboard"
+                    exact
+                    component={AdminDashboard}
+                />
             </Switch>
         </BrowserRouter>
     );
