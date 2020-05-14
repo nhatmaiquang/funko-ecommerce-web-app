fe9d79087b10a8631fbefff7d0140c50f757c533
Mai Quang Nhat <quangnhat224@gmail.com>
2020-05-14 14:33:42 +0300

    user profile

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -3,16 +3,18 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-const { userById, read, update } = require("../controllers/user");
+const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
-router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
+router.get('/secret', requireSignin, (req, res) => {
     res.json({
-        user: req.profile
+        user: 'got here yay'
     });
 });
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
+
 router.param('userId', userById);
 
 module.exports = router;
